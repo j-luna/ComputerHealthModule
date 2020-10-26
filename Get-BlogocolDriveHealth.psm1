d68f1b270a3a63b7132c754c63c701f2f5d2456b
Joseph Lunaburg <j.lunaburg@gmail.com>
2020-10-26 18:53:01 -0500

    Finished the general begin/process/end structure

--- Get-BlogocolDriveHealth.psm1
+++ Get-BlogocolDriveHealth.psm1
@@ -26,93 +26,108 @@ function Get-BlogocolDriveHealth {
         [Parameter(
             Position=1)]
         [String]$logname
-    )    
+    )
+    
+    BEGIN {
+        $alldiskreport = [System.Collections.ArrayList]@()
+    }
 
-    $disks = Get-PhysicalDisk
-    $tempmax = 3220 # Corresponds to around 50C -- recommended that hard drives do not reach/go beyond this point
+    PROCESS {
 
-    foreach ($disk in $disks) {
-        Write-Output "Checking disk: $($disk.FriendlyName) ($($disk.MediaType.ToString())) from computer: $computername ..."
+        $disks = Get-PhysicalDisk
+        $tempmax = 3220 # Corresponds to around 50C -- recommended that hard drives do not reach/go beyond this point
+    
+        foreach ($disk in $disks) {
+            Write-Output "Checking disk: $($disk.FriendlyName) ($($disk.MediaType.ToString())) from computer: $computername ..."
         
-        $problemsfound = 0
-        $diskstatus = $disk | Get-StorageReliabilityCounter
+            $problemsfound = 0
+            $diskstatus = $disk | Get-StorageReliabilityCounter
 
-        $healthstatus = $disk.HealthStatus
-        $operationalstatus = $disk.OperationalStatus
+            $healthstatus = $disk.HealthStatus
+            $operationalstatus = $disk.OperationalStatus
 
-        $smartstatus = (Get-CimInstance `
-            -ComputerName $computername `
-            -Namespace 'root\WMI' `
-            -ClassName 'MSStorageDriver_FailurePredictStatus').PredictFailure
+            $smartstatus = (Get-CimInstance `
+                -ComputerName $computername `
+                -Namespace 'root\WMI' `
+                -ClassName 'MSStorageDriver_FailurePredictStatus').PredictFailure
 
-        $temp = $diskstatus.Temperature
-        $tempKelvin = 0
-        $tempCelsius = 0
+            $temp = $diskstatus.Temperature
+            $tempKelvin = 0
+            $tempCelsius = 0
 
-        if ($temp -eq 0) {
-            Write-Output "Cannot get temperature reading of disk from WMI. Please contact the manufacturer for more information on interfacing with the disk temperature.`n"
-        }
-        else {
-            $tempKelvin = ($temp) / 10.0
-            $tempCelsius = $tempKelvin - 273.15
-        }
+            if ($temp -eq 0) {
+                Write-Output "Cannot get temperature reading of disk from WMI. Please contact the manufacturer for more information on interfacing with the disk temperature.`n"
+            }
+            else {
+                $tempKelvin = ($temp) / 10.0
+                $tempCelsius = $tempKelvin - 273.15
+            }
 
-        if ($healthstatus -ne "Healthy") {
-            $problemsfound++
-            $healthreport = "Warning -- status: $healthstatus"
-        }
-        else {
-            $healthreport = "Passed"
-        }
+            if ($healthstatus -ne "Healthy") {
+                $problemsfound++
+                $healthreport = "Warning -- status: $healthstatus"
+            }
+            else {
+                $healthreport = "Passed"
+            }
 
-        if ($operationalstatus -ne "OK") {
-            $problemsfound++
-            $operationalreport = "Warning -- status: $operationalstatus"
-        }
-        else {
-            $operationalreport = "Passed"
-        }
+            if ($operationalstatus -ne "OK") {
+                $problemsfound++
+                $operationalreport = "Warning -- status: $operationalstatus"
+            }
+            else {
+                $operationalreport = "Passed"
+            }
 
-        if ($temp -eq 0) {
-            $tempreport = "Inconclusive"
-        }
-        elseif ($temp -ge $tempmax) {
-            $problemsfound++
-            $tempreport =  "Warning -- current temp: ($tempCelsius C)"
-        }
-        else {
-            $tempreport = "Passed -- current temp: $tempCelsius C"
-        }
+            if ($temp -eq 0) {
+                $tempreport = "Inconclusive"
+            }
+            elseif ($temp -ge $tempmax) {
+                $problemsfound++
+                $tempreport =  "Warning -- current temp: ($tempCelsius C)"
+            }
+            else {
+                $tempreport = "Passed -- current temp: $tempCelsius C"
+            }
         
-        if ($smartstatus -eq $true) {
-            $problemsfound++
-            $smartreport = "Failed"
-        }
-        else {
-            $smartreport = "Passed"
-        }
+            if ($smartstatus -eq $true) {
+                $problemsfound++
+                $smartreport = "Failed"
+            }
+            else {
+                $smartreport = "Passed"
+            }
 
-        $reportfordisk = "For disk $($disk.FriendlyName) on $computername --
-        Health status check: $healthreport
-        Operational status check: $operationalreport
-        Temperature check: $tempreport
-        S.M.A.R.T. check: $smartreport"
+            $reportfordisk = "For disk $($disk.FriendlyName) on $computername --
+            Health status check: $healthreport
+            Operational status check: $operationalreport
+            Temperature check: $tempreport
+            S.M.A.R.T. check: $smartreport`n"
 
-        Write-Verbose $reportfordisk
-        if ($problemsfound -gt 0) {
-            $resultfordisk = "$problemsfound problems detected on the disk. Refer to logs for more information
-            or choose the '-Verbose' parameter."
+            Write-Verbose $reportfordisk
+            if ($problemsfound -gt 0) {
+                $resultfordisk = "$problemsfound problems detected on the disk. Refer to logs for more information
+                or choose the '-Verbose' parameter."
+            }
+            else {
+                $resultfordisk = "No problems found. All tests passed."
+            }
+            
+            $alldiskreport.Add($resultfordisk)
         }
-        else {
-            $resultfordisk = "No problems found. All tests passed."
-        }
-        
+    }
+
+    END {
         if ($logname -ne $NULL) {
             try { 
-                $reportfordisk.toString() + "`n" + $resultfordisk.toString() | Out-File $logname -Force -Append
+                foreach ($diskreport in $alldiskreport) {
+                    $reportfordisk.toString() + "`n" + $resultfordisk.toString() | Out-File $logname -Force -Append
+                }
             }catch {
-                Write-Host "Write to log failed. Please enter a valid log path."
+            Write-Host "Write to log failed. Please enter a valid log path."
             }  
         }
+
+        return $logname
     }
 }
