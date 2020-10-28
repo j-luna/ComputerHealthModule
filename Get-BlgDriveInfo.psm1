99d4774651986dec3a818eff21a5186ce17c928a
Joseph Lunaburg <j.lunaburg@gmail.com>
2020-10-28 00:09:03 -0500

    Modified the original Get-BlogocolDriveHealth.psm1 file (changed name to Get-BlgDriveInfo.psm1) in order to solely retrieve key information from physical disks, partitions, and volumes. The idea here is to use this particular function to retrieve information, and then create another function to process the reporting (i.e. what to do if there is a specific problem).

---
+++ Get-BlgDriveInfo.psm1
@@ -0,0 +1,49 @@
+
+# -----------------------------------------------------------
+# Credit to Ansgar Wiechers for the logical structure for extracting key properties.
+# Uses WMI queries to tie specific physical disks to their corresponding partitions,
+# and those partitions to their corresponding volumes.
+# Source: https://stackoverflow.com/questions/31088930/combine-get-disk-info-and-logicaldisk-info-in-powershell
+# -----------------------------------------------------------
+
+function Get-BlgDriveHealth {
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            ValueFromPipeline=$true,
+            Position=0
+        )]
+        [String]$computername='localhost'
+    )
+        
+    Get-CimInstance Win32_DiskDrive | ForEach-Object {
+        $disk = $_
+        $partitions = "ASSOCIATORS OF " +
+                "{Win32_DiskDrive.DeviceID='$($disk.DeviceID)'} " +
+                "WHERE AssocClass = Win32_DiskDriveToDiskPartition"
+        Get-CimInstance -Query $partitions | ForEach-Object {
+            $partition = $_
+            $drives = "ASSOCIATORS OF " +
+                "{Win32_DiskPartition.DeviceID='$($partition.DeviceID)'} " +
+                "WHERE AssocClass = Win32_LogicalDiskToPartition"
+            Get-CimInstance -Query $drives | ForEach-Object {
+                New-Object -Type PSCustomObject -Property @{
+                    Disk                    = $disk.DeviceID
+                    PhysicalDiskHealth      = $disk.Status
+                    DiskSerialNumber        = $disk.SerialNumber
+                    DiskSize                = $disk.Size
+                    DiskModel               = $disk.Model
+                    Partition               = $partition.Name
+                    PartitionSize           = $partition.Size
+                    DriveLetter             = $_.DeviceID
+                    VolumeName              = $_.VolumeName
+                    Size                    = $_.Size
+                    FreeSpace               = $_.FreeSpace
+                    VolumeSerialNumber      = $_.VolumeSerialNumber
+                    VolumeOperationalStatus = ($_ | Select-Object *,@{l="DriveLetter";e={ ([char[]]$_.DeviceId)[0]} } | Get-Volume).OperationalStatus
+                    VolumeHealthyStatus     = ($_ | Select-Object *,@{l="DriveLetter";e={ ([char[]]$_.DeviceId)[0]} } | Get-Volume).HealthStatus
+                }
+            }
+        }
+    }
+}
