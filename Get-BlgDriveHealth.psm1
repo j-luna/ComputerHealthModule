17deeeeaed20e35c0622fdb06b5f95df43f81586
Joseph Lunaburg <j.lunaburg@gmail.com>
2020-10-28 18:52:09 -0500

    Finished driving function that receives output from Get-BlgDriveStatus and exports results to a logfile

---
+++ Get-BlgDriveHealth.psm1
@@ -0,0 +1,44 @@
+function Get-BlgDriveHealth {
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipeline=$true,
+            Position=0)]
+        [Alias("Computer")]
+        [String[]]$computers
+    )
+
+    BEGIN {
+        $problemcomputers = [System.Collections.ArrayList]@()
+        New-Item -Path "C:\drivelogs" -ItemType Directory -Force
+        $logfile = New-Item -Path "C:\drivelogs\BlgDriveHealth" -ItemType File -Force
+    }
+
+    PROCESS {
+        foreach ($computer in $computers) {
+            $problems = Get-BlgDriveStatus -Name $computer
+            $problemcomputers.Add($problems)
+        }
+    }
+
+    END {
+        
+        #$problemcomputers | Out-File $logfile -Append
+        
+        foreach ($problemcomputer in $problemcomputers) {
+            Write-Output "Issues found on $($problemcomputer.ComputerName) on $($problemcomputer.DriveLetter)" | Out-File $logfile -Append -Force
+            Write-Output "
+                VolumeName         : $($problemcomputer.VolumeName)
+                FreeSpace          : $($problemcomputer.SpaceUsed)
+                S.M.A.R.T. status  : $($problemcomputer.PhysicalDiskHealth)
+                Drive status       : $($problemcomputer.VolumeOperationalStatus)
+                Drive health       : $($problemcomputer.VolumeHealthStatus) 
+            " | Out-File $logfile -Append -Force
+        }
+
+        Write-Host "Problem drives written to C:\drivelogs\BlgDriveHealth"
+    }
+}
+
+
