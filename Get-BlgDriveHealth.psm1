569c5251ca1775a4f44b81860967c4999ac603fd
Joseph Lunaburg <j.lunaburg@gmail.com>
2020-10-28 21:08:44 -0500

    Merged all separate functions into one file

--- Get-BlgDriveHealth.psm1
+++ Get-BlgDriveHealth.psm1
@@ -37,4 +37,81 @@ function Get-BlgDriveHealth {
     }
 }
 
+function Get-BlgDriveStatus {
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Mandatory=$true,
+            ValueFromPipeline=$true,
+            Position=0)]
+        [Alias("Name")]
+        [String]$computername
+    )
+    
+    BEGIN {}
+    PROCESS {
+        
+        Get-BlgDriveInfo -Name $computername | ForEach-Object {
+            $smarthealth = $_.PhysicalDiskHealth
+            $opstatus    = $_.VolumeOperationalStatus
+            $healthstatus = $_.VolumeHealthStatus
+            $availablespace = [math]::Round( ((1 - ($freespace / $totalsize) ) * 100 ),2)
+
+            if (
+                ($smarthealth -ne 'OK') -or
+                ($opstatus -ne 'OK') -or
+                ($healthstatus -ne 'Healthy') -or
+                ($availablespace -le 10)
+            ) { $_ | Select-Object *,@{n='SpaceUsed';e={ ([string]$availablespace + '%') } } }         
+        }    
+    }
+
+    END{}
+
+}
+
+function Get-BlgDriveInfo {
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            ValueFromPipeline=$true,
+            Position=0
+        )]
+        [Alias("Name")]
+        [String]$computername='localhost'
+    )
+    
+    Get-CimInstance Win32_DiskDrive | ForEach-Object {
+        $disk = $_
+        $partitions = "ASSOCIATORS OF " +
+                "{Win32_DiskDrive.DeviceID='$($disk.DeviceID)'} " +
+                "WHERE AssocClass = Win32_DiskDriveToDiskPartition"
+        Get-CimInstance -Query $partitions | ForEach-Object {
+            $partition = $_
+            $drives = "ASSOCIATORS OF " +
+                "{Win32_DiskPartition.DeviceID='$($partition.DeviceID)'} " +
+                "WHERE AssocClass = Win32_LogicalDiskToPartition"
+            Get-CimInstance -Query $drives | ForEach-Object {
+                New-Object -Type PSCustomObject -Property @{
+                    ComputerName            = $computername
+                    Disk                    = $disk.DeviceID
+                    PhysicalDiskHealth      = $disk.Status
+                    DiskSerialNumber        = $disk.SerialNumber
+                    DiskSize                = $disk.Size
+                    DiskModel               = $disk.Model
+                    Partition               = $partition.Name
+                    PartitionSize           = $partition.Size
+                    DriveLetter             = $_.DeviceID
+                    VolumeName              = $_.VolumeName
+                    Size                    = $_.Size
+                    FreeSpace               = $_.FreeSpace
+                    VolumeSerialNumber      = $_.VolumeSerialNumber
+                    VolumeOperationalStatus = ($_ | Select-Object *,@{l="DriveLetter";e={ ([char[]]$_.DeviceId)[0]} } | Get-Volume).OperationalStatus
+                    VolumeHealthStatus      = ($_ | Select-Object *,@{l="DriveLetter";e={ ([char[]]$_.DeviceId)[0]} } | Get-Volume).HealthStatus
+                }
+            }
+        }
+    }
+}
+
 
